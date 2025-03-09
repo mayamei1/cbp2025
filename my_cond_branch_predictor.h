2e65a20e58a6d45e4bbfe7ff066a59d22eccbc37
Maya Mei <gmei@ncsu.edu>
2025-03-08 20:01:43 -0500

    two level

--- my_cond_branch_predictor.h
+++ my_cond_branch_predictor.h
@@ -4,17 +4,22 @@
 #include <array>
 #include <stdlib.h>
 
-using bht_t = uint16_t;
+using bht_entry_t = uint16_t;
+using lbh_entry_t = uint16_t;
 
 static const size_t MAX_SIZE = 192000;
 static const size_t SATURATION_BITS = 2;
-static const size_t BHT_SIZE = MAX_SIZE / SATURATION_BITS;
-static const bht_t SATURATION_VALUE = (1 << SATURATION_BITS) - 1;
-static const bht_t NOT_TAKEN_MAX = (1 << (SATURATION_BITS - 1)) - 1;
+static const size_t BRANCH_HISTORY_LENGTH = 3;
+
+static const size_t NUM_HISTORY_PATTERNS = 1 << BRANCH_HISTORY_LENGTH;
+static const lbh_entry_t HISTORY_MASK = NUM_HISTORY_PATTERNS - 1;
+static const size_t NUM_BHT_ENTRIES = MAX_SIZE / (SATURATION_BITS * NUM_HISTORY_PATTERNS);
+static const bht_entry_t SATURATION_VALUE = (1 << SATURATION_BITS) - 1;
+static const bht_entry_t NOT_TAKEN_MAX = (1 << (SATURATION_BITS - 1)) - 1;
 
 class BHT {
     private:
-        std::array<bht_t, BHT_SIZE> table;
+        std::array<bht_entry_t, NUM_BHT_ENTRIES> table;
     public:
         void setup() {
             table.fill(NOT_TAKEN_MAX);
@@ -28,6 +33,24 @@ class BHT {
         }
 };
 
+class TwoLevelBHT {
+    private:
+        std::array<lbh_entry_t, NUM_HISTORY_PATTERNS> LBH;
+        std::array<BHT, NUM_HISTORY_PATTERNS> BHTs;
+    public:
+        void setup() {
+            LBH.fill(0);
+            for (BHT &b : BHTs) b.setup();
+        }
+        bool predict(int idx) {
+            return BHTs[LBH[idx]].predict(idx);
+        }
+        void update(int idx, bool val) {
+            BHTs[LBH[idx]].update(idx, val);
+            LBH[idx] = ((LBH[idx] << 1) | val) & HISTORY_MASK;
+        }
+};
+
 /* Size of Predictor: 192 KB */
 class BitSaturatingBranchPredictor
 {
@@ -38,11 +61,11 @@ class BitSaturatingBranchPredictor
         }
 
         bool predict(uint64_t pc) {
-            return bht.predict(pc % BHT_SIZE);
+            return bht.predict(pc % NUM_BHT_ENTRIES);
         }
 
         void update(uint64_t pc, bool taken) {
-            bht.update(pc % BHT_SIZE, taken);
+            bht.update(pc % NUM_BHT_ENTRIES, taken);
         }
 };
 // =================
