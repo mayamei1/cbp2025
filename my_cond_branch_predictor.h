793000ce7f40b69937610e02e868c38eaffd59b3
Maya Mei <gmei@ncsu.edu>
2025-03-09 19:13:10 -0400

    updated to best branch history length

--- my_cond_branch_predictor.h
+++ my_cond_branch_predictor.h
@@ -9,7 +9,7 @@ using lbh_entry_t = uint16_t;
 
 static const size_t MAX_SIZE = 192000;
 static const size_t SATURATION_BITS = 2;
-static const size_t BRANCH_HISTORY_LENGTH = 1;
+static const size_t BRANCH_HISTORY_LENGTH = 3;
 
 static const size_t NUM_HISTORY_PATTERNS = 1 << BRANCH_HISTORY_LENGTH;
 static const lbh_entry_t HISTORY_MASK = NUM_HISTORY_PATTERNS - 1;
