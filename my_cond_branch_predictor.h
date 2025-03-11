97954daf80aa5465ea6e6d0c76e2bdaae88e19c8
Maya Mei <gmei@ncsu.edu>
2025-03-11 19:35:54 -0400

    gshare with right align

--- my_cond_branch_predictor.h
+++ my_cond_branch_predictor.h
@@ -9,12 +9,11 @@ using gbh_t = uint64_t;
 
 static const size_t MAX_SIZE = 192000;
 static const size_t SATURATION_BITS = 2;
-static const size_t BRANCH_HISTORY_LENGTH = 1;
+static const size_t BRANCH_HISTORY_LENGTH = 16;
 
 /* MAX_SIZE = NUM_HISTORY_PATTERNS * NUM_BHT_ENTRIES * SATURATION_BITS + BRANCH_HISTORY_LENGTH */
-static const size_t NUM_HISTORY_PATTERNS = 1 << BRANCH_HISTORY_LENGTH;
-static const gbh_t HISTORY_MASK = NUM_HISTORY_PATTERNS - 1;
-static const size_t NUM_BHT_ENTRIES = (MAX_SIZE - BRANCH_HISTORY_LENGTH) / (NUM_HISTORY_PATTERNS * SATURATION_BITS);
+static const gbh_t HISTORY_MASK = (1 << BRANCH_HISTORY_LENGTH) - 1;
+static const size_t NUM_BHT_ENTRIES = (MAX_SIZE - BRANCH_HISTORY_LENGTH) / SATURATION_BITS;
 
 static const bht_entry_t SATURATION_VALUE = (1 << SATURATION_BITS) - 1;
 static const bht_entry_t NOT_TAKEN_MAX = (1 << (SATURATION_BITS - 1)) - 1;
@@ -36,45 +35,34 @@ class BHT {
         }
 };
 
-class TwoLevelBHT {
-    private:
-        /* BRANCH_HISTORY_LENGTH */
-        gbh_t GBH;
-        /* NUM_HISTORY_PATTERNS * NUM_BHT_ENTRIES * SATURATION_BITS */
-        std::array<BHT, NUM_HISTORY_PATTERNS> BHTs;
-    public:
-        void setup() {
-            GBH = 0;
-            for (BHT &b : BHTs) b.setup();
-        }
-        bool predict(int idx) {
-            return BHTs[GBH].predict(idx);
-        }
-        void update(int idx, bool val) {
-            BHTs[GBH].update(idx, val);
-            GBH = ((GBH << 1) | val) & HISTORY_MASK;
-        }
-};
-
 /*
 ====================================================================================================
     Size of Predictor: 192 KB
-    NUM_HISTORY_PATTERNS * NUM_BHT_ENTRIES * SATURATION_BITS + BRANCH_HISTORY_LENGTH
+    NUM_BHT_ENTRIES * SATURATION_BITS + BRANCH_HISTORY_LENGTH
 ====================================================================================================
 */
-class gselect
+class gshare
 {
-    TwoLevelBHT bht;
+    /* NUM_BHT_ENTRIES * SATURATION_BITS */
+    BHT bht;
+    /* BRANCH_HISTORY_LENGTH */
+    gbh_t gbh;
+    private:
+        int get_index(uint64_t pc) {
+            return ((pc >> 2) % NUM_BHT_ENTRIES) ^ gbh;
+        }
     public:
         void setup() {
             bht.setup();
+            gbh = 0;
         }
 
         bool predict(uint64_t pc) {
-            return bht.predict((pc >> 2) % NUM_BHT_ENTRIES);
+            return bht.predict(get_index(pc));
         }
         void update(uint64_t pc, bool taken) {
-            bht.update((pc >> 2) % NUM_BHT_ENTRIES, taken);
+            bht.update(get_index(pc), taken);
+            gbh = ((gbh << 1) | taken) & HISTORY_MASK;
         }
 };
 // =================
@@ -82,4 +70,4 @@ class gselect
 // =================
 
 #endif
-static gselect predictor;
+static gshare predictor;
