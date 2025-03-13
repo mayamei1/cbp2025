d88c2ef60d488ac57658c8212b0da3501bf33dc9
Maya Mei <gmei@ncsu.edu>
2025-03-13 12:10:14 -0400

    bimodal

--- my_cond_branch_predictor.h
+++ my_cond_branch_predictor.h
@@ -9,18 +9,44 @@
 #define MAX_PRED_BITS 192000
 #endif
 
+#ifndef NUM_BHR_INDEX_BITS
+#define NUM_BHR_INDEX_BITS 16
+#endif
+
+#ifndef NUM_SATURATION_BITS
+#define NUM_SATURATION_BITS 2
+#endif
+
+#define BHR_SIZE (1 << NUM_BHR_INDEX_BITS)
+#define BHR_INDEX_MASK (BHR_SIZE - 1)
+#define MAX_SATURATION_VALUE ((1 << NUM_SATURATION_BITS) - 1)
+#define MAX_NOT_TAKEN (MAX_SATURATION_VALUE >> 1)
+
 class MyPredictor {
+    private:
+    std::array<uint8_t, BHR_SIZE> bhr;
     public:
     void setup() {
-
+        // Check # of bits used
+        int num_bits = BHR_SIZE * NUM_SATURATION_BITS;
+        assert(num_bits <= MAX_PRED_BITS);
+        
+        // BHR initialization
+        bhr.fill(MAX_NOT_TAKEN);
     }
 
     bool predict(uint64_t pc) {
-        return false;
+        // Calculate index
+        int index = (pc >> 2) & BHR_INDEX_MASK;
+        bool pred = bhr[index] > MAX_NOT_TAKEN;
+        return pred;
     }
 
     void update(uint64_t pc, bool taken) {
-
+        // Calculate index
+        int index = (pc >> 2) & BHR_INDEX_MASK;
+        if (taken && bhr[index] != MAX_SATURATION_VALUE) bhr[index]++;
+        else if (!taken && bhr[index] != 0) bhr[index]--;
     }
 };
 
