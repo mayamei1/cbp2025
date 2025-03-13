6550c8066d842d622abcad59efefbb3b7acc8a4f
Maya Mei <gmei@ncsu.edu>
2025-03-13 13:12:06 -0400

    local two-level

--- my_cond_branch_predictor.h
+++ my_cond_branch_predictor.h
@@ -10,43 +10,52 @@
 #endif
 
 #ifndef NUM_BHR_INDEX_BITS
-#define NUM_BHR_INDEX_BITS 16
+#define NUM_BHR_INDEX_BITS 12
 #endif
 
 #ifndef NUM_SATURATION_BITS
 #define NUM_SATURATION_BITS 2
 #endif
 
+#ifndef BRANCH_HISTORY_LENGTH
+#define BRANCH_HISTORY_LENGTH 4
+#endif
+
 #define BHR_SIZE (1 << NUM_BHR_INDEX_BITS)
 #define BHR_INDEX_MASK (BHR_SIZE - 1)
 #define MAX_SATURATION_VALUE ((1 << NUM_SATURATION_BITS) - 1)
 #define MAX_NOT_TAKEN (MAX_SATURATION_VALUE >> 1)
+#define BRANCH_HISTORY_SIZE (1 << BRANCH_HISTORY_LENGTH)
+#define BRANCH_HISTORY_MASK (BRANCH_HISTORY_SIZE - 1)
 
 class MyPredictor {
     private:
-    std::array<uint8_t, BHR_SIZE> bhr;
+    std::array<std::array<uint8_t, BHR_SIZE>, BRANCH_HISTORY_SIZE> bhr;
+    std::array<uint16_t, BHR_SIZE> lbh;
     public:
     void setup() {
         // Check # of bits used
-        int num_bits = BHR_SIZE * NUM_SATURATION_BITS;
-        assert(num_bits <= MAX_PRED_BITS);
+        int bhr_count = BRANCH_HISTORY_SIZE * BHR_SIZE * NUM_SATURATION_BITS;
+        int lbh_count = BHR_SIZE * BRANCH_HISTORY_LENGTH;
+        assert((bhr_count + lbh_count) <= MAX_PRED_BITS);
         
         // BHR initialization
-        bhr.fill(MAX_NOT_TAKEN);
+        for (auto b : bhr) b.fill(MAX_NOT_TAKEN);
     }
 
     bool predict(uint64_t pc) {
         // Calculate index
         int index = (pc >> 2) & BHR_INDEX_MASK;
-        bool pred = bhr[index] > MAX_NOT_TAKEN;
+        bool pred = bhr[lbh[index]][index] > MAX_NOT_TAKEN;
         return pred;
     }
 
     void update(uint64_t pc, bool taken) {
         // Calculate index
         int index = (pc >> 2) & BHR_INDEX_MASK;
-        if (taken && bhr[index] != MAX_SATURATION_VALUE) bhr[index]++;
-        else if (!taken && bhr[index] != 0) bhr[index]--;
+        if (taken && bhr[lbh[index]][index] != MAX_SATURATION_VALUE) bhr[lbh[index]][index]++;
+        else if (!taken && bhr[lbh[index]][index] != 0) bhr[lbh[index]][index]--;
+        lbh[index] = ((lbh[index] << 1) | taken) & BRANCH_HISTORY_MASK;
     }
 };
 
